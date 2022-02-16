a9e7a1be086c6c3792f558d33429e6f0031ddfdc
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-15 22:42:09 -0500

    add logic to handle no tag parameter

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,19 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', function (req, res) {
+  res.status(400).json({error: 'Tags parameter is required'});
+});
+
 router.get('/:tags/:sortBy?/:direction?', function (req, res) {
-  //sortBy optional, default val of "id"
-  // direction, default "asc"
-  // tags=history,tech
-  const {tags, sortBy, direction} = req.params;
+  const {tags = ' ', sortBy = 'id', direction = 'asc'} = req.params;
+
+  const errorString = '';
+
+  if (!tags) {
+    console.log('eher2');
+    res.json('error');
+  }
   //change tagsArr
   const tagsArr = tags.split(',');
 
@@ -19,34 +27,26 @@ router.get('/:tags/:sortBy?/:direction?', function (req, res) {
       .catch(err => console.log(err));
   }
 
-  const urlArr = tagsArr.map(
-    tag =>
-      `https://api.hatchways.io/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`
-  );
-  console.log(
-    tagsArr.map(tag => {
-      console.log(tag);
-      return axios.get(
-        `https://api.hatchways.io/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`
-      );
-    })
-  );
-
   Promise.all(
-    tagsArr.map(tag => {
-      console.log(tag);
-      console.log(sortBy);
-      console.log(direction);
-      return axios.get(
+    tagsArr.map(tag =>
+      axios.get(
         `https://api.hatchways.io/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`
+      )
+    )
+  )
+    .then(all => {
+      [one, two] = all;
+      const allPosts = [...one.data.posts, ...two.data.posts];
+
+      // filter out duplicate blog posts
+      const ids = allPosts.map(el => el.id);
+      const filtered = allPosts.filter(
+        ({id}, index) => !ids.includes(id, index + 1)
       );
+
+      res.status(200).send(filtered);
     })
-  ).then(all => {
-    [one, two] = all;
-    console.log([...one.data.posts, ...two.data.posts]);
-    res.status(200).send([...one.data.posts, ...two.data.posts]);
-  });
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
-// at least one tag specified
